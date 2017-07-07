b88f81e07ba245ac435223d90724a4e87536efcd
jacobladams <jacobladams@gmail.com>
2017-07-07 08:51:50 -0500

    started adding prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,22 +2,23 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const uuidv4 = require('uuid/v4');
 // const _ = require('lodash'),
 
 
 
 module.exports = class extends Generator {
-	// constructor() {
-	// 	super(arguments);
-	// 	this.argument('appname', { type: String, required: true });
-    //     this.appname = _.kebabCase(this.appname);
+	constructor() {
+		super(arguments);
+		this.argument('appname', { type: String, required: true });
+        //this.appname = _.kebabCase(this.appname);
 
-    //     this.option('includeutils', {
-    //        desc: 'Optionally includes Angular-UI Utils library.',
-    //        type: Boolean,
-    //        default: false
-    //     });
-	// }
+        // this.option('includeutils', {
+        //    desc: 'Optionally includes Angular-UI Utils library.',
+        //    type: Boolean,
+        //    default: false
+        // });
+	}
 
   prompting() {
     // Have Yeoman greet the user.
@@ -25,12 +26,34 @@ module.exports = class extends Generator {
       'Welcome to the incredible ' + chalk.red('generator-rocket-start-angularjs') + ' generator!'
     ));
 
+    // const prompts = [{
+    //   type: 'confirm',
+    //   name: 'someAnswer',
+    //   message: 'Would you like to enable this option?',
+    //   default: true
+	// }];
+
+
     const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      type: 'input',
+      name: 'version',
+      message: 'Version Number',
+      default: '1.0.0'
+	},{
+      type: 'input',
+      name: 'author',
+      message: 'Author',
+	},{
+      type: 'input',
+      name: 'angularModuleName',
+	  message: 'AngularJS Module Name',
+	  default: _.kebabCase(this.appname)
+	},{
+      type: 'input',
+      name: 'angularAppComponentName',
+	  message: 'AngularJS App Component Name',
+	  default: 'app'
+	}];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -44,22 +67,45 @@ module.exports = class extends Generator {
 	// 	this.destinationPath('dummyfile.txt'),
 	// 	{foo: 'hello world'}
 	// );
+
+
+	// var createGuid = function() {
+	// 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	// 	var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	// 	return v.toString(16);
+	// 	});
+	// }
+
+	// const projectGuid = createGuid();
+	const vsProjectGuid = uuidv4();
+
+
 	console.log('template path: '+ this.templatePath()+ '/**')
 	this.fs.copyTpl(
-      	this.templatePath()+ '/**',
-			this.destinationPath(),
+		this.templatePath() + '/**',
+		this.destinationPath(),
 		{
-			// foo: 'hello world',
-			// bar: 'hello' + this.props.someAnswer,
-			projectName: 'rocket-start-angularjs',
-			version: '0.0.1',
-			author: 'jacobladams',
-			angularModuleName: 'rocket-start',
-			angularAppComponentName: 'app',
-			angularAppComponentTag: 'app'
-			// angularAppComponentTag: _.kebabCase('fooBarBazApp')
-
+			projectName: this.appname,
+			version: this.props.version,
+			author: this.props.author,
+			angularModuleName: this.props.angularModuleName,
+			angularAppComponentName: this.props.angularAppComponentName,
+			angularAppComponentTag: _.kebabCase(this.props.angularAppComponentName)
 		}
+
+		// 	this.destinationPath(),
+		// {
+		// 	// foo: 'hello world',
+		// 	// bar: 'hello' + this.props.someAnswer,
+		// 	projectName: 'rocket-start-angularjs',
+		// 	version: '0.0.1',
+		// 	author: 'jacobladams',
+		// 	angularModuleName: 'rocket-start',
+		// 	angularAppComponentName: 'app',
+		// 	angularAppComponentTag: 'app'
+		// 	// angularAppComponentTag: _.kebabCase('fooBarBazApp')
+
+		// }
 		// {
 		// 	foo: 'hello world',
 		// 	bar: 'hello' + this.props.someAnswer,
